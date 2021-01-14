51dfcc06bb7bebea2c2d72e70ada592bee4f656c
BiplabMazumder2021 <76530534+BiplabMazumder2021@users.noreply.github.com>
2021-01-13 21:01:00 -0500

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -16,7 +16,7 @@ job('NodeJS Docker example') {
         dockerBuildAndPublish {
             repositoryName('bmaz2021/second')
             tag('${GIT_REVISION,length=9}')
-            registryCredentials('dockerhub')
+            registryCredentials('bmaz2021')
             forcePull(false)
             forceTag(false)
             createFingerprints(false)
